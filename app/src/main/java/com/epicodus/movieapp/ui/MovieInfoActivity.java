ba7b6b8d277dbaa0c2415cb2fa73e4a887367a40
Luca Quatela and Nick Lyman <nick@>
2016-07-07 16:13:56 -0700

    Added listview to activity_movie_info.xml and set up ArrayAdapter and UiThread for looping through list of movie titles.

--- app/src/main/java/com/epicodus/movieapp/ui/MovieInfoActivity.java
+++ app/src/main/java/com/epicodus/movieapp/ui/MovieInfoActivity.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.epicodus.movieapp.R;
+import com.epicodus.movieapp.model.Movie;
 import com.epicodus.movieapp.services.TmdbService;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
@@ -17,10 +23,15 @@ import okhttp3.Response;
 public class MovieInfoActivity extends AppCompatActivity {
     public static final String TAG = MovieInfoActivity.class.getSimpleName();
 
+    @Bind(R.id.movieTitlesListView) ListView mListView;
+
+    public ArrayList<Movie> mMovies = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String userInput = intent.getStringExtra("userInput");
@@ -41,7 +52,24 @@ public class MovieInfoActivity extends AppCompatActivity {
             public void onResponse(Call call, Response response) throws IOException {
                 try {
                     String jsonData = response.body().string();
-                    Log.v(TAG, jsonData);
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                        mMovies = tmdbService.processResults(response);
+
+                        MovieInfoActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                String[] movieTitles = new String[mMovies.size()];
+                                for (int i = 0; i < movieTitles.length; i++) {
+                                    movieTitles[i] = mMovies.get(i).getTitle();
+                                }
+                                ArrayAdapter adapter = new ArrayAdapter(MovieInfoActivity.this,
+                                        android.R.layout.simple_list_item_1, movieTitles);
+                                mListView.setAdapter(adapter);
+                            }
+                        });
+                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
